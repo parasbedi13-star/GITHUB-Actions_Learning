ce4bc956ea8f318888a710c64f304658b9909fc9
parasbedi13-star <parasbedi13@gmail.com>
2026-09-12 09:16:31 +0530

    intilize code

---
+++ pipeline_infra/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+    features {}
+  }
+  variable "rgs" {}
+
+  resource "azurerm_resource_group" "name" {
+
+    for_each = var.rgs
+    name = each.value.name
+    location = each.value.location
+    
+  }
---
+++ pipeline_infra/terraform.tfvars
@@ -0,0 +1,6 @@
+rgs = {
+    rg-1 = {
+        name = "RG-dev-01"
+        location = "Eastus"
+    }
+}
